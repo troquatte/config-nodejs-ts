188f30b5e1594de4151cecfc6e63b885e3204784
Dener Troquatte <dener@vidafullstack.com.br>
2021-08-29 13:08:08 -0300

    Melhorias na no first controller

--- src/app/controller/FirstController.ts
+++ src/app/controller/FirstController.ts
@@ -1,13 +1,11 @@
 import { Request, Response } from "express";
 
-export class FirstController{
-
-  public home(req:Request, res:Response) {
+class FirstController {
+  public home(req: Request, res: Response) {
     return res.json({
-      response: 'Hello World'
+      response: "Hello World",
     });
   }
-
 }
 
 export const firstController = new FirstController();
